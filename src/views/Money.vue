5c65eb6504e732630928adabe052ee1f5fc16be7
HHuangYQ <1135782479@qq.com>
2022-06-24 18:37:22 +0800

    Money.vue使用vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,40 +1,45 @@
 <template>
   <div>
     <layout class-prefix="layout">
-      <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-      <Types :value.sync="record.type"/>
+      <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+      <Types :value.sync="record.type" />
       <div class="notes">
-        <FormItem field-name="备注"
-                  placeholder="在这里输入备注"
-                  @update:value="onUpdateNotes"/>
+        <FormItem
+          field-name="备注"
+          placeholder="在这里输入备注"
+          @update:value="onUpdateNotes"
+        />
       </div>
-      <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+      <Tags />
     </layout>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import FormItem from '@/components/Money/FormItem.vue';
-import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
-
+import Vue from "vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import FormItem from "@/components/Money/FormItem.vue";
+import Tags from "@/components/Money/Tags.vue";
+import { Component } from "vue-property-decorator";
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad},
+  components: { Tags, FormItem, Types, NumberPad },
+  computed: {
+    recordList() {
+      return this.$store.state.recordList;
+    },
+  },
 })
 export default class Money extends Vue {
-  tags = store.tagList;
-  recordList = store.recordList;
   record: RecordItem = {
-    tags: [], notes: '', type: '-', amount: 0
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
   };
-
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
+  created() {
+    this.$store.commit('fetchRecords')
   }
 
   onUpdateNotes(value: string) {
@@ -42,17 +47,16 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    store.createRecord(this.record)
+    this.$store.commit("createRecord", this.record);
   }
-};
+}
 </script>
 <style lang="scss">
-
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
 }
-.notes{
-  padding: 12px 0 ;
+.notes {
+  padding: 12px 0;
 }
 </style>
